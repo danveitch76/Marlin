5f8eebd299e64254ea9aa538561cf57991c60a75
danveitch76 <45300973+danveitch76@users.noreply.github.com>
2021-02-21 23:13:49 +0000

    Enabled Arc Support
    Enabled Arc Support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -138,7 +138,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 (BF) - 15-FEB-2021"
+#define CUSTOM_MACHINE_NAME "Ender-3 (BF) - 21-FEB-2021"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
